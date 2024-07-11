0e4c394a33cb1f9f1e09b7ca059ab272bb638bd2
sahilgudape <118040429+sahilgudape@users.noreply.github.com>
2024-07-11 18:53:25 +0530

    Delete Working of Javascript/Javascript Fundamentals 3 directory

--- Working of Javascript/Javascript Fundamentals 3/5areaMath.js
+++
@@ -1,3 +0,0 @@
-const radius = 2;
-const areaOfCircle = Math.PI*Math.pow(radius,2);
-console.log(areaOfCircle);
--- Working of Javascript/Javascript Fundamentals 3/6reverseString.js
+++
@@ -1,9 +0,0 @@
-function reverseString(input) {
-    return input.split('').reverse().join('','');
-  }
-  
-  const originalString = "Hello, world!";
-  console.log('original string: ',originalString);
-  const reversedString = reverseString(originalString);
-  console.log('reversed string: ',reversedString);
-  
